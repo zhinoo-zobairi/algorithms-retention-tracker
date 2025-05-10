a3a85fa3a499ba6a8f518f46992e8c5bc808e093
Zhinoo Zobairi <zobairyjino@gmail.com>
2025-05-10 23:18:40 +0200

    Review Day 14 for insertion sort in python

--- algorithms/insertion_sort/review_day_14.py
+++ algorithms/insertion_sort/review_day_14.py
@@ -7,14 +7,14 @@ Complete the implementation below to reinforce your understanding.
 '''
 
 def insertion_sort(items):
-    """
-    Implementation of Insertion Sort
-    
-    TODO: Implement the algorithm from memory to reinforce your learning
-    """
-    # Your implementation here
-    pass
-
+    for i in range(1, len(items)):
+        temp = items[i]
+        j = i - 1
+        while j >= 0 and items[j] > temp:
+            items[j+1] = items[j]
+            j-=1
+        items[j+1] = temp
+    return items
 if __name__ == "__main__":
     test_data = [5, 2, 9, 1, 5, 6]
     result = insertion_sort(test_data)
