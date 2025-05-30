62a3091b40c97b5239b600cee551c1b88d12b137
Zhinoo Zobairi <zobairi1998@gmail.com>
2025-05-30 16:12:12 +0200

    implemented iterative merge sort using reusable merge() with slicing

--- algorithms/iterative_merge_sort/code.py
+++ algorithms/iterative_merge_sort/code.py
@@ -1,4 +1,18 @@
 # Iterative Merge Sort implementation
+from merge.code import merge
+def iterative_merge_sort(items):
+    size = 1
+    while size < len(items): # the last useful merge happens when the length of the sublists are in fact less than the original list's length; hence < and not <=
+        for start in range(0, len(items), 2*size):
+            mid = start + size - 1
+            end = min(mid + size, len(items) - 1) # When handling the last chunk in a list with uneven size, we may walk past the borders if we don't catch it
+            first_half = items[start:mid+1]
+            second_half = items[mid+1:end+1]
+            merged = merge(first_half, second_half)
+            items[start:end+1] = merged  # OVERWRITE the sorted chunk
+        size *=2
 
-def iterative_merge_sort():
-    pass
+if __name__ == "__main__":
+    items = [38, 27, 43, 3, 9, 82, 10, 5]
+    iterative_merge_sort(items)
+    print(items)
--- algorithms/merge/code.py
+++ algorithms/merge/code.py
@@ -12,11 +12,11 @@ def merge(items_1, items_2):
             new_items.append(items_2[j])
             j+=1
     # handling leftovers
-    while i <= len(items_1):
+    while i < len(items_1):
         new_items.append(items_1[i])
         i += 1
     
-    while j <= len(items_2):
+    while j < len(items_2):
         new_items.append(items_2[j])
         j+=1
 
