1296073fdc26001a3b35300db8f4576de2e48346
Zhinoo Zobairi <zobairyjino@gmail.com>
2025-06-02 00:18:56 +0200

    Added 3rd day review for Iterative Merge Sort in C by importing already wrtitten Merge

--- algorithms/iterative_merge_sort/review_day_1.c
+++ algorithms/iterative_merge_sort/review_day_1.c
@@ -8,23 +8,42 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#define MIN(a,b) (((a)<(b))?(a):(b))
+
+#include "../merge/merge.h"
 
 /**
  * Implementation of Iterative Merge Sort
  * TODO: Implement the algorithm from memory to reinforce your learning
+ * I need 2 for loops: once for each size: I am basically saying at each size, you have to do a series of actions
+ * A second loop for that series of actions I just mentioned: Compute the start, mid and end => prepare it for merge Algorithm) as long as the start is less than the length.
  */
-void iterative_merge_sort(int items[], int size) {
-    /* Your implementation here */
+void iterative_merge_sort(int items[], int length) {
+    for (int size = 1; size < length; size *= 2)
+    {
+       for (int start = 0; start < length; start += 2 * size) // I let 'start' hit risky values like 'start' = 'length'-1, but I catch invalid ranges by clamping with min(...) in the next step. We say start += 2 * size, because the next start must jump past: 1. The 'size' elements from the left half, 2. The 'size' elements from the right half.
+       {
+       int mid = MIN(start + size - 1, length - 1);
+       int end = MIN(start + 2 * size - 1, length - 1);
+
+        if (mid < end) {
+            merge(items, start, mid, end);
+        }
+       }
+       
+    }
+   
+    
 }
 
 int main() {
     int test_data[] = {5, 2, 9, 1, 5, 6};
-    int size = sizeof(test_data) / sizeof(test_data[0]);
+    int length = sizeof(test_data) / sizeof(test_data[0]);
     
-    iterative_merge_sort(test_data, size);
+    iterative_merge_sort(test_data, length);
     
     printf("Result: ");
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < length; i++) {
         printf("%d ", test_data[i]);
     }
     printf("\n");
