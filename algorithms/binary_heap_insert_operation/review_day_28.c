fead95358fe57f2cde97cc84da82e1880c79edc6
Zhinoo Zobairi <zobairi1998@gmail.com>
2025-06-02 18:58:44 +0200

    Added day 28 review of insertion operation in Binary Heap in C

--- algorithms/binary_heap_insert_operation/review_day_28.c
+++ algorithms/binary_heap_insert_operation/review_day_28.c
@@ -12,16 +12,25 @@
 /**
  * Implementation of Binary Heap Insert Operation
  * TODO: Implement the algorithm from memory to reinforce your learning
+ * This version is different with day 3, where your get a list where the new element is already inserted and you have to only restore heap hierarchy.
  */
-void binary_heap_insert_operation(int items[], int size) {
-    /* Your implementation here */
+void binary_heap_insert_operation(int items[], int *size, int new_item) {
+    int i = *size;
+    int inserted_value = new_item;
+    while (i > 0 && items[(i - 1) / 2] < inserted_value)
+    {
+        items[i] = items[(i - 1) / 2];
+        i = (i - 1) / 2;
+    }
+    items[i] = inserted_value;
+    (*size)++;
 }
 
 int main() {
-    int test_data[] = {5, 2, 9, 1, 5, 6};
-    int size = sizeof(test_data) / sizeof(test_data[0]);
+    int test_data[100] = {9, 5, 6, 1, 2, 3};  // A valid max-heap
+    int size = 6;
     
-    binary_heap_insert_operation(test_data, size);
+    binary_heap_insert_operation(test_data, &size, 10);
     
     printf("Result: ");
     for (int i = 0; i < size; i++) {
