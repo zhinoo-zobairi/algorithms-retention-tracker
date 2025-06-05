c7089a173790487074ea205c05626937b96f6ac8
Zhinoo Zobairi <zobairi1998@gmail.com>
2025-06-05 10:37:50 +0200

    Reviewing Merge in JS on day 8

--- algorithms/merge/review_day_7.js
+++ algorithms/merge/review_day_7.js
@@ -12,12 +12,44 @@
  * @returns {Array} - The processed array
  * TODO: Implement the algorithm from memory to reinforce your learning
  */
-function merge(items) {
-    // Your implementation here
+function merge(items, low, mid, high) {
+    let i = low; 
+    let j = mid + 1;
+    let k = 0;
+    let temp = [];
+    while (i <= mid && j <= high) {
+        if (items[i] <= items[j]) {
+            temp[k] = items[i];
+            k++;
+            i++;
+        } else {
+            temp[k] = items[j];
+            k++;
+            j++;
+        }
+    }
+    // Leftovers:
+    while (i <= mid) {
+        temp[k] = items[i];
+        i++;
+        k++;
+    }
+    while (j <= high) {
+        temp[k] = items[j];
+        j++;
+        k++;
+    }
+    k = 0;
+    // let start = low; // would be better to create a new variable to "preserve the boundary", but since we are done with "low" and don't need him hanging around, we can mutate it.
+    while (low <= high) {
+       items[low] = temp[k]
+       k++;
+       low++;
+    }
     return items;
 }
 
 // Test the implementation
-const testData = [5, 2, 9, 1, 5, 6];
-const result = merge(testData);
+const testData = [2, 4, 7, 1, 5, 9];
+const result = merge(testData, 0, 2, 5); // Result: 1,2,4,5,7,9
 console.log(`Result: ${result}`);
