3e567b840c7378ce9411232de3a75235c2d12901
Zhinoo Zobairi <zobairyjino@gmail.com>
2025-06-08 17:20:09 +0200

    Added day 28th review for Heap Deletion in C

--- algorithms/heap_deletion_and_sort_max_heap/review_day_28.c
+++ algorithms/heap_deletion_and_sort_max_heap/review_day_28.c
@@ -14,20 +14,45 @@
  * TODO: Implement the algorithm from memory to reinforce your learning
  */
 void heap_deletion_and_sort_max_heap(int items[], int size) {
-    /* Your implementation here */
+    int deleted_elements[100];
+    int deleted_index = 0;
+    while (size > 0)
+    {    
+    int deleted_element = items[0];
+    deleted_elements[deleted_index++] = deleted_element;
+    items[0] = items[size - 1];
+    size--;
+    int i = 0;
+    while (2 * i + 1 < size){
+        int left_child = 2 * i + 1;
+        int right_child = 2 * i + 2;
+        int larger_child = left_child; // I initially made the mistake of having int larger_child = items[left_child] which got me stuck, where I had to assign the index to current i, and I couldn't tell if it was right or left child's index.
+        if (right_child < size && items[right_child] > items[left_child]){
+            larger_child = right_child;
+        }
+        if (items[i] < items[larger_child]){
+            int temp = items[i];
+            items[i] = items[larger_child];
+            items[larger_child] = temp;
+            i = larger_child;
+            }else
+            {
+                break;
+            }
+            
+        }        
+    }
+    printf("Sorted array (ascending): ");
+    for (int k = deleted_index - 1; k >= 0; k--) {
+    printf("%d ", deleted_elements[k]);
+}
 }
 
 int main() {
-    int test_data[] = {5, 2, 9, 1, 5, 6};
-    int size = sizeof(test_data) / sizeof(test_data[0]);
-    
-    heap_deletion_and_sort_max_heap(test_data, size);
-    
-    printf("Result: ");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", test_data[i]);
-    }
-    printf("\n");
-    
+    int heap[] = {50, 30, 40, 10, 5, 20, 35};
+    int size = sizeof(heap) / sizeof(heap[0]);
+
+    heap_deletion_and_sort_max_heap(heap, size);
+
     return 0;
 }
