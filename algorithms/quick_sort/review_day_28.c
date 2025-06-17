c0e88bb8574c6430e8c1af118b4de0408223bb24
Zhinoo Zobairi <zobairyjino@gmail.com>
2025-06-17 15:52:06 +0200

    Reviewing Quick Sort with 2 days of delay on day 30, instead of day 28

--- algorithms/quick_sort/review_day_28.c
+++ algorithms/quick_sort/review_day_28.c
@@ -13,21 +13,61 @@
  * Implementation of Quick Sort
  * TODO: Implement the algorithm from memory to reinforce your learning
  */
-void quick_sort(int items[], int size) {
-    /* Your implementation here */
+
+ void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+ }
+
+ int partition(int items[], int l, int h){
+    int pivot = items[l];
+    int i = l + 1;
+    int j = h;
+
+    while (i<=j)
+    {
+        while (i<=j && items[i] <= pivot)
+        {
+            i++;
+        }
+        while (i<=j && items[j] >= pivot)
+        {
+            j--;
+        }
+        if (i <= j)
+        {
+            swap(&items[i], &items[j]);
+        }
+        else
+        {
+            break;
+        }
+        
+        
+    }
+    swap(&items[j], &items[l]); // If you swap with pivot, then pivot gets overwritten with items[j] (useless, pivot is a dead local copy!)
+
+    return j;
+ }
+
+
+void quick_sort(int items[], int low, int high) {
+    if (low < high) {
+        int p = partition(items, low, high);
+        quick_sort(items, low, p);
+        quick_sort(items, p + 1, high);
+    }
 }
 
 int main() {
-    int test_data[] = {5, 2, 9, 1, 5, 6};
-    int size = sizeof(test_data) / sizeof(test_data[0]);
-    
-    quick_sort(test_data, size);
-    
-    printf("Result: ");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", test_data[i]);
-    }
-    printf("\n");
-    
+    int arr[] = {9, 3, 7, 1, 8, 2, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    quick_sort(arr, 0, size - 1);
+
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+
     return 0;
 }
