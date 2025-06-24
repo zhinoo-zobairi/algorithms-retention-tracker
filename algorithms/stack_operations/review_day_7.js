a85eaaedede5b88d41cd7d1c12527874c81b45e1
Zhinoo Zobairi <zobairyjino@gmail.com>
2025-06-24 17:57:45 +0200

    Stack operations in JS

--- algorithms/stack_operations/review_day_7.js
+++ algorithms/stack_operations/review_day_7.js
@@ -12,12 +12,43 @@
  * @returns {Array} - The processed array
  * TODO: Implement the algorithm from memory to reinforce your learning
  */
-function stack_operations(items) {
-    // Your implementation here
-    return items;
+class Stack {
+    constructor(size) {
+        this.top = -1;
+        this.array = new Array(size); // preallocate space
+        this.size = size;
+    }
+
+stack_operations_push(data) {
+    if (this.top == this.size -1) {
+        throw new Error("Stack Overflow");
+    } else {
+        this.top ++;
+        this.array[this.top] = data;
+    }
+}
+stack_operations_pop() {
+    if (this.top == -1) {
+        throw new Error("Stack Underflow"); 
+    } else {
+        const value = this.array[this.top];
+        this.top --;
+        return value;
+    }
 }
 
-// Test the implementation
-const testData = [5, 2, 9, 1, 5, 6];
-const result = stack_operations(testData);
-console.log(`Result: ${result}`);
+stack_operations_display(){
+    for (let index = 0; index <= this.top; index++) {
+        console.log("the element is", this.array[index])
+    }
+}
+}
+
+
+const stack = new Stack(5);
+stack.stack_operations_push(19);
+stack.stack_operations_push(42);
+stack.stack_operations_push(7);
+stack.stack_operations_display();      
+console.log(stack.stack_operations_pop()); 
+stack.stack_operations_display();
