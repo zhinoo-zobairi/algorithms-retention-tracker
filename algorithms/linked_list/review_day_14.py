eefa469a4dad5cc3a5e9005ed4b7b1567414b244
Zhinoo Zobairi <zobairyjino@gmail.com>
2025-06-25 15:55:40 +0200

    Review Day 14 for Linked List in Py

--- algorithms/linked_list/review_day_14.py
+++ algorithms/linked_list/review_day_14.py
@@ -6,16 +6,43 @@ This is your review exercise for Linked List.
 Complete the implementation below to reinforce your understanding.
 '''
 
-def linked_list(items):
-    """
-    Implementation of Linked List
-    
-    TODO: Implement the algorithm from memory to reinforce your learning
-    """
-    # Your implementation here
-    pass
 
-if __name__ == "__main__":
-    test_data = [5, 2, 9, 1, 5, 6]
-    result = linked_list(test_data)
-    print(f"Result: {result}")
+class Node():
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+    def display(self):
+        current = self
+        while current != None:
+            print(current.value)
+            current = current.next
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, value):
+        if self.head is None:
+            self.head = Node(value) # where we connect self.head in the LinkedList class to a Node object
+        else:
+            current = self.head
+            while current.next != None:
+                current = current.next
+            new = Node(value)
+            current.next = new
+        
+    def prepend(self, value):
+            new = Node(value)
+            new.next = self.head
+            self.head = new
+    
+    def display(self):
+        current = self.head
+        while current is not None:
+            print(current.value)
+            current = current.next
+
+ll = LinkedList()
+ll.append(20)
+ll.append(30)
+ll.prepend(10)
+ll.display()
