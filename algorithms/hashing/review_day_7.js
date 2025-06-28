49596eb6561d2360d06e077a81738a4c2a22aa4d
Zhinoo Zobairi <zobairyjino@gmail.com>
2025-06-28 13:58:26 +0200

    Review Day 7 for Hashing

--- algorithms/hashing/review_day_7.js
+++ algorithms/hashing/review_day_7.js
@@ -12,12 +12,64 @@
  * @returns {Array} - The processed array
  * TODO: Implement the algorithm from memory to reinforce your learning
  */
-function hashing(items) {
-    // Your implementation here
-    return items;
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
 }
 
-// Test the implementation
-const testData = [5, 2, 9, 1, 5, 6];
-const result = hashing(testData);
-console.log(`Result: ${result}`);
+class LinkedList {
+    constructor() {
+        this.head =  null;
+    }
+    insert(value){
+        const node = new Node(value);
+        node.next = this.head;
+        this.head = node;
+    }
+    print(){
+        let current = this.head;
+        let output = '';
+        while (current != null) {
+            output += `${current.value} -> `;
+            current = current.next;
+            }
+        output += 'null';
+        console.log(output);
+    }
+}
+
+class HashTable {
+    constructor(size) {
+        this.size = size;
+        this.buckets = [];
+        for (let index = 0; index <= this.size - 1; index++) {
+            this.buckets[index] = new LinkedList();
+        }
+    }
+    hashFunction(value){
+        return value % this.size;
+    }
+    insert(value){
+        const index = this.hashFunction(value);
+        this.buckets[index].insert(value);
+    }
+    print() {
+    for (let i = 0; i < this.size; i++) {
+        process.stdout.write(`Bucket ${i}: `);
+        this.buckets[i].print();
+        }
+    }
+}
+
+const table = new HashTable(10);
+
+table.insert(5);
+table.insert(15);
+table.insert(25);
+table.insert(7);
+table.insert(17);
+table.insert(9);
+
+table.print();
