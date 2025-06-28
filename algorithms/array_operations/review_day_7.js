4b1e1d6c0ab017a33d5e16d68566e8d6c0b40c6e
Zhinoo Zobairi <zobairyjino@gmail.com>
2025-06-28 12:06:26 +0200

    Review Day 7 for Array Operations

--- algorithms/array_operations/review_day_7.js
+++ algorithms/array_operations/review_day_7.js
@@ -12,12 +12,67 @@
  * @returns {Array} - The processed array
  * TODO: Implement the algorithm from memory to reinforce your learning
  */
-function array_operations(items) {
-    // Your implementation here
-    return items;
+
+class MyArray{
+    constructor(size){
+        this.array = new Array(size); // preallocate space for future items
+        this.size = size;
+        this.length = 0;
+    }
+    initializeFromArray(input){
+        if (input.length > this.size) {
+        throw new Error("Input array exceeds allocated size.");
+    }  
+        for (let index = 0; index < input.length; index++) {
+            this.array[index] = input[index];
+            this.length += 1;
+        }
+    }
+    sort(){
+       for (let index = 1; index < this.length; index++) {
+        const key = this.array[index];
+        let j = index - 1;
+        while (j >= 0 && this.array[j] > key) {
+            this.array[j + 1] = this.array[j];
+            j--;
+        }
+        this.array[j + 1] = key;
+       }
+    }
+
+    removeDuplicates() {
+    const unique = [];
+
+    for (let i = 0; i < this.length; i++) {
+        let isDuplicate = false;
+
+        for (let j = 0; j < unique.length; j++) {
+            if (this.array[i] === unique[j]) {
+                isDuplicate = true;
+                break;
+            }
+        }
+
+        if (!isDuplicate) {
+            unique.push(this.array[i]);
+        }
+    }
+
+    this.array = unique;
+    this.length = unique.length;
+}
+    print() {
+        for (let i = 0; i < this.length; i++) {
+            console.log(`Element ${i}: ${this.array[i]}`);
+        }
+}
 }
 
-// Test the implementation
+
 const testData = [5, 2, 9, 1, 5, 6];
-const result = array_operations(testData);
-console.log(`Result: ${result}`);
+const array_new = new MyArray(10);
+array_new.initializeFromArray(testData);
+array_new.print();           
+array_new.sort();          
+array_new.removeDuplicates(); 
+array_new.print();  
