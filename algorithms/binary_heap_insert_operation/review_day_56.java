92c9a837fa9a97b13b8e45e58988bb06e78672c1
Zhinoo Zobairi <zobairi1998@gmail.com>
2025-06-30 17:43:18 +0200

    Review Day 56 for Binary Heap Insert Operation in Java

--- algorithms/binary_heap_insert_operation/review_day_56.java
+++ algorithms/binary_heap_insert_operation/review_day_56.java
@@ -8,18 +8,51 @@
 
 import java.util.Arrays;
 
-public class BinaryHeapInsertOperation {
-    /**
-     * Implementation of Binary Heap Insert Operation
-     * TODO: Implement the algorithm from memory to reinforce your learning
-     */
-    public static void binary_heap_insert_operation(int[] items) {
-        // Your implementation here
+public class BinaryHeapInsertOperationReview {
+    int[] heap;
+    int size
+    int count;
+
+    BinaryHeapInsertOperationReview(int size){
+        this.size = size;
+        this.heap = new int[size]; // fixed-size array
+        this.count = 0;
     }
-    
+
+    public void binary_heap_insert(int new_value) {
+        if (this.count == this.size){
+            System.out.println("Heap is full!");
+        } else {
+            int i = this.count + 1; // First available index in a 1-based heap
+            this.heap[i] = new_value;
+            while (i >= 1 && new_value < this.heap[i / 2]){
+                this.heap[i] = this.heap[i/2];
+                i = i/2;
+            }
+            this.heap[i] = new_value;
+            this.count++;
+        }
+    }
+    public void binary_heap_build(int[] new_values) {
+        if (this.size >= new_values.length) {
+            for (int i = 0; i < new_values.length; i++) {
+                this.binary_heap_insert(new_values[i]);
+            }
+        } else {
+    System.out.println("Error: Input array is too large for the heap size.");
+        }
+    }
+
+    public void binary_heap_display() {
+        for (int i = 1; i < this.count + 1; i++){
+            System.out.print(this.heap[i] + " ");
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        int[] testData = {5, 2, 9, 1, 5, 6};
-        binary_heap_insert_operation(testData);
-        System.out.println("Result: " + Arrays.toString(testData));
+        BinaryHeapInsertOperationReview my_heap = new BinaryHeapInsertOperationReview(10);
+        heap.binary_heap_build(new int[]{4, 7, 2, 9, 1});
+        my_heap.binary_heap_insert(5);
+        my_heap.binary_heap_display();
     }
 }
