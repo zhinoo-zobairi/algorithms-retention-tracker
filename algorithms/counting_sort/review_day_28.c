85134d5326b9de8cc2ba2ac7a7c0781c098b69e1
Zhinoo Zobairi <zobairyjino@gmail.com>
2025-07-06 23:55:31 +0200

    Review Day 28 for Counting Sort in C

--- algorithms/counting_sort/review_day_28.c
+++ algorithms/counting_sort/review_day_28.c
@@ -9,25 +9,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * Implementation of Counting Sort
- * TODO: Implement the algorithm from memory to reinforce your learning
- */
+
+int maximum(int items[], int size) {
+    if (size <= 0) {
+        return -1; 
+    }
+    int max = items[0];
+    for (int i = 1; i < size; i++) { 
+        if (items[i] > max) {
+            max = items[i];
+        }
+    }
+    return max;
+}
+
+
 void counting_sort(int items[], int size) {
-    /* Your implementation here */
+    int max = maximum(items, size);
+    if (max < 0) {
+        printf("Error: Negative values are not supported.\n");
+        return;
+    }
+
+    int aux[max + 1];
+    for (int i = 0; i < max + 1; i++) {
+        aux[i] = 0;
+    }
+
+    for (int i = 0; i < size; i++) {
+        if (items[i] < 0) {
+            printf("Error: Negative values are not supported.\n");
+            return;
+        }
+        aux[items[i]]++;
+    }
+
+    int i = 0;
+    for (int j = 0; j < max + 1; j++) {
+        while (aux[j] > 0) {
+            items[i] = j;
+            aux[j]--;
+            i++;
+        }
+    }
 }
 
 int main() {
     int test_data[] = {5, 2, 9, 1, 5, 6};
     int size = sizeof(test_data) / sizeof(test_data[0]);
-    
+
     counting_sort(test_data, size);
-    
+
     printf("Result: ");
     for (int i = 0; i < size; i++) {
         printf("%d ", test_data[i]);
     }
     printf("\n");
-    
+
     return 0;
 }
