0dfd9a60ee7892636ec80785e7b2c8087aea7d92
Zhinoo Zobairi <zobairyjino@gmail.com>
2025-07-10 21:51:20 +0200

    Review Day 28 for Recursive Merge Sort with 6 days of delay

--- algorithms/recursive_merge_sort/review_day_28.c
+++ algorithms/recursive_merge_sort/review_day_28.c
@@ -8,26 +8,33 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "../merge/merge.h"
 
 /**
  * Implementation of Recursive Merge Sort
- * TODO: Implement the algorithm from memory to reinforce your learning
  */
-void recursive_merge_sort(int items[], int size) {
-    /* Your implementation here */
+void recursive_merge_sort(int items[], int l, int h) {
+    if (l < h) {
+        int mid = (l + h) / 2;
+
+        recursive_merge_sort(items, l, mid);
+        recursive_merge_sort(items, mid + 1, h);
+
+        merge(items, l, mid, h);
+    }
 }
 
 int main() {
     int test_data[] = {5, 2, 9, 1, 5, 6};
     int size = sizeof(test_data) / sizeof(test_data[0]);
-    
-    recursive_merge_sort(test_data, size);
-    
+
+    recursive_merge_sort(test_data, 0, size - 1);
+
     printf("Result: ");
     for (int i = 0; i < size; i++) {
         printf("%d ", test_data[i]);
     }
     printf("\n");
-    
+
     return 0;
 }
