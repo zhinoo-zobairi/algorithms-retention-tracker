774409e9fb31b79bba42b66edfd8edb69e5158f6
Zhinoo Zobairi <zobairi1998@gmail.com>
2025-09-17 12:14:35 +0200

    Review Day 112 for Merge

--- algorithms/merge/review_day_112.js
+++ algorithms/merge/review_day_112.js
@@ -12,12 +12,42 @@
  * @returns {Array} - The processed array
  * TODO: Implement the algorithm from memory to reinforce your learning
  */
-function merge(items) {
-    // Your implementation here
-    return items;
+// This is not in-place:
+function merge(items_1, items_2) {
+    let i = 0;
+    let j = 0;
+    let mixed_items = [];
+    let k = 0;
+    while (i < items_1.length && j < items_2.length) {
+        if (items_1[i] >= items_2[j]) {
+            mixed_items[k] = items_2[j];
+            j++;
+        } else{
+            mixed_items[k] = items_1[i];
+            i++;
+        }
+        k++;
+    }
+    if (i < items_1.length) {
+        while (i < items_1.length) {
+            mixed_items[k] = items_1[i];
+            i++;
+            k++;
+        }
+    }
+    if (j < items_2.length) {
+        while (j < items_2.length) {
+            mixed_items[k] = items_2[j];
+            j++;
+            k++;
+        }
+    }
+    return mixed_items;
 }
 
 // Test the implementation
-const testData = [5, 2, 9, 1, 5, 6];
-const result = merge(testData);
+const testData_1 = [1, 2, 10];
+const testData_2 = [7, 9, 11, 20];
+
+const result = merge(testData_1, testData_2);
 console.log(`Result: ${result}`);
