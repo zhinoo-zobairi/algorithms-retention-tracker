a8e3c3678324f10eddd4d8d20f224d06c9470dae
Zhinoo Zobairi <zobairi1998@gmail.com>
2025-12-08 18:18:54 +0100

    Add insertion sort implementation, day 224
    Implemented the insertion sort algorithm to sort an array of integers.

--- algorithms/insertion_sort/review_day_224.c
+++ algorithms/insertion_sort/review_day_224.c
@@ -14,9 +14,16 @@
  * TODO: Implement the algorithm from memory to reinforce your learning
  */
 void insertion_sort(int items[], int size) {
-    /* Your implementation here */
+    for (int i = 1; i < size; i++){ // the left side of && is evaluated first, so when j becomes -1, this tries to evaluate items[j] before checking j >= 0. That is out-of-bounds access and undefined behavior.
+        int key = items[i];
+        int j = i - 1;
+        while (j >= 0 && key < items[j]){
+            items[j+1] = items[j];
+            j--;
+        }
+        items[j+1] = key
+    }
 }
-
 int main() {
     int test_data[] = {5, 2, 9, 1, 5, 6};
     int size = sizeof(test_data) / sizeof(test_data[0]);
